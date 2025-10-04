614c5791a3b7ea99a2115d7a2f9706e104a480e4
Enes Eldeş <ns.eldes51@gmail.com>
2025-10-04 14:55:33 +0300

    Init react

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>GRIDEA</h1>
+      <div>Deneme Sitesi</div>
+    </>
+  )
+}
+
+export default App
